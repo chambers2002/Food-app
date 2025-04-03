c6d2a1bde970edb633bd1edd63424edc59c709bb
chambers2002 <declanchambers666@gmail.com>
2025-04-03 03:47:07 +0100

    Created home.html

--- userapp/views.py
+++ userapp/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Hello this is our home page for our Food App</h1>')
+    return render(request, 'userapp/home.html', {'title': 'Welcome'})
+
+def about(request):
+    return HttpResponse('<h1>Hello this is our About page for our Food App</h1>')
+
+def contact(request):
+    return HttpResponse('<h1>Hello this is our Contact page for our Food App</h1>')
 # Create your views here.
 
