f799510ac0d711f4f25305a11580699a529a0a24
chambers2002 <declanchambers666@gmail.com>
2025-04-03 01:52:53 +0100

    Updated urls to see the userapp page

--- userapp/views.py
+++ userapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse('<h1>Hello this is our home page for our Food App</h1>')
 # Create your views here.
+
