a4c554344b9314f4ce2268d5db836f6d391e0c12
chambers2002 <declanchambers666@gmail.com>
2025-04-11 07:30:40 +0100

    Created basic register page

--- foodapp/urls.py
+++ foodapp/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from registration import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('userapp.urls')),
+    path('register/', views.register, name='register'),
 ]
---
+++ registration/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('userapp:home')
+        else:
+            messages.warning(request, 'Unable to create account.')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form, 'title': 'Student Registration'})
+# Create your views here.
