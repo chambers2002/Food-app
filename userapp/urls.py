5c14ad48b2517de9a3527ed2e1a0b1c0d52fb736
chambers2002 <declanchambers666@gmail.com>
2025-04-23 21:51:35 +0100

    Replaced the standard box and created a New box when logged in to go to different main pages on this app

--- userapp/urls.py
+++ userapp/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('about', views.about, name = 'about'),
     path('contact', views.contact, name = 'contact'),
     path('meals/', views.meals, name='meals'),
+    path('mealsuggestions/', views.meal_suggestions, name='meal_suggestions'),
+    path('budgetgroceries/', views.budget_groceries, name='budget_groceries'),
+    path('dietpreferences/', views.diet_preferences, name='diet_preferences'),
+    path('ecomeals/', views.eco_meals, name='eco_meals'),
 ]
