fe85aee6c798c78b33bce0ad7ce7bfdb3aaf3ab3
chambers2002 <declanchambers666@gmail.com>
2025-05-09 05:31:27 +0100

    Added in a Profile Picture to the Profile Page

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    User = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
+    
+    def _str_(self):
+        return f'{self.user.first_name} {self.user.last_name}'
 
 # Create your models here.
